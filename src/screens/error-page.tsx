2017bddcbf737ef7663246f82492ebf0fe2b7813
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2023-12-15 15:30:18 +0700

    FEAT: set up color scale

--- src/screens/error-page.tsx
+++ src/screens/error-page.tsx
@@ -23,7 +23,7 @@ export default function ErrorPage() {
           label="Go Back"
           onClick={() => navigate(-1)}
           type="button"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="flex w-full justify-center rounded-md bg-kl-blue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-kl-blue-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-kl-blue"
         />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "kl-blue": "#4678F3",
+        "kl-blue-light": "#5CA6F7",
+      },
+    },
   },
   plugins: [],
 };
